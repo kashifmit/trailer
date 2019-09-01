9eb6adbdbed334c80d64d843f160da7d262ceb2c
Kashif Hussain <logiccanvas@gmail.com>
2019-09-02 00:50:19 +0500

    update profile, customer details page designed

--- resources/views/users/update_profile.blade.php
+++ resources/views/users/update_profile.blade.php
@@ -2,31 +2,103 @@
 
 @section('content')
 @include('flash::message')
-<div class="card">
-    <div class="card-header">{{ __('Update Profile') }}</div>
-    <div class="card-body">
-        {!! Form::open(array('method' => 'put', 'route' => array('update.profile'), 'class' => 'form', 'files'=>true)) !!}
-        {!! Form::hidden('id', Auth::user()->id) !!}
-            <div class="form-body">
-    <div class="form-group row">
-            {!! Form::label('name', 'Name', ['class' => 'bold']) !!}                    
-            {!! Form::text('name', Auth::user()->name, array('class'=>'form-control', 'id'=>'name', 'placeholder'=>'Name')) !!}
-            {!! APFrmErrHelp::showErrors($errors, 'name') !!}
+<div class="page">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+
+                <header class="heading">
+                    <div class="h3 title mb-2">{{ __('Account Information') }}</div>
+                    <h5 class="title">{{ __('Account Email / Username and Password') }}</h5>
+                </header>
+
+                <div class="content">
+                    {!! Form::open(array('method' => 'put', 'route' => array('update.profile'), 'class' => 'form', 'files'=>true)) !!}
+                    {!! Form::hidden('id', Auth::user()->id) !!}
+                        <div class="form-body">
+                            <div class="form-group row mb-5">
+                                {!! Form::label('email', 'Email Address', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                                <div class="col-md-4">
+                                    {!! Form::text('name', Auth::user()->email, array('class'=>'form-control form-control-radius', 'id'=>'email', 'placeholder'=>'Name')) !!}
+                                    {!! APFrmErrHelp::showErrors($errors, 'name') !!}
+                                </div>
+                                <div class="col-md-4">
+                                    <button type="button" class="btn btn-primary">Save Email</button>
+                                </div>
+                            </div>
+
+
+                            <header class="heading">
+                                <h5 class="title">{{ __('Personal Information') }}</h5>
+                            </header>
+
+                            <div class="form-group row">
+                                {!! Form::label('fName', 'First Name', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                                <div class="col-md-4">
+                                    <input type="text" name="fName" id="fName" class="form-control form-control-radius" placeholder="First Name">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                {!! Form::label('lName', 'Last Name', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                                <div class="col-md-4">
+                                    <input type="text" name="lName" id="lName" class="form-control form-control-radius" placeholder="Last Name">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                {!! Form::label('organization', 'Organization', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                                <div class="col-md-4">
+                                    <select name="organization" id="organization" class="form-control form-control-radius">
+                                        <option value="one">One</option>
+                                        <option value="two">Two</option>
+                                        <option value="three">Three</option>
+                                        <option value="four">Four</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-5">
+                                {!! Form::label('location', 'Location', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                                <div class="col-md-4">
+                                    <select name="location" id="location" class="form-control form-control-radius">
+                                        <option value="one">One</option>
+                                        <option value="two">Two</option>
+                                        <option value="three">Three</option>
+                                        <option value="four">Four</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <button type="button" class="btn btn-primary">Update Personal Information</button>
+                                </div>
+                            </div>
+
+                            <header class="heading">
+                                <h5 class="title">{{ __('Password Update') }}</h5>
+                            </header>
+
+                            <div class="form-group row">
+                                {!! Form::label('password', 'New Password', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                                <div class="col-md-4">
+                                    <input type="password" name="password" id="password" class="form-control form-control-radius" placeholder="New Password">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                {!! Form::label('c_password', 'Confirm Password', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                                <div class="col-md-4">
+                                    <input type="password" name="c_password" id="c_password" class="form-control form-control-radius" placeholder="Confirm Password">
+                                </div>
+                                <div class="col-md-4">
+                                    {!! Form::button('Update Password', array('class'=>'btn btn-primary', 'type'=>'submit')) !!}
+                                </div>
+                            </div>
+
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+
+            </div>
         </div>
-        <div class="form-group row">
-            {!! Form::label('password', 'New Password', ['class' => 'bold']) !!}                    
-            <input type="password" name="password" id="password" class="form-control" placeholder="New Password">
-        </div>
-        <div class="form-group row">
-            {!! Form::label('c_password', 'Confirm Password', ['class' => 'bold']) !!}                    
-            <input type="password" name="c_password" id="c_password" class="form-control" placeholder="Confirm Password">
-        </div>
-        <div class="form-actions">
-            {!! Form::button('Submit <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>', array('class'=>'btn btn-large btn-primary', 'type'=>'submit')) !!}
-        </div>
-    </div>
-        {!! Form::close() !!}
-    </div>
+
 </div>
 @endsection
 
