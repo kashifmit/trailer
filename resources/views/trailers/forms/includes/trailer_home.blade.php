ea089b33f4f60916d9df6359813570095fa53a84
Kashif Hussain <logiccanvas@gmail.com>
2019-09-26 12:00:28 +0500

    responsive menu list for each page set, homepage, trailer details home tab responsive settinngs

--- resources/views/trailers/forms/includes/trailer_home.blade.php
+++ resources/views/trailers/forms/includes/trailer_home.blade.php
@@ -6,13 +6,13 @@
     <div class="mb-5">
         {!! Form::open(array('method' => 'GET', 'route' => 'trailer.list', 'class' => 'form', 'files'=>true, 'id' => 'search-by-trailer-number')) !!}
         <div class="form-group row mb-0">
-            <label for="TrailerSerialNo" class="col-md-3 col-form-label text-md-right">{{ __('Enter Trailer Number') }}</label>
-            <div class="col-md-3">
-                <div class="form-control-wrap search">
+            <label for="TrailerSerialNo" class="col-lg-3 col-form-label text-lg-right">{{ __('Enter Trailer Number') }}</label>
+            <div class="col-md-6 col-lg-3">
+                <div class="form-control-wrap search mb-2 mb-md-0">
                     <input type="text" class="form-control form-control-radius" name="TrailerSerialNo" id="TrailerSerialNo" autocomplete="off" placeholder="Trailer Number" value="{{\Request::get('TrailerSerialNo')}}">
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6 col-lg-3">
                 {!! Form::button('Find', array('class'=>'search-by-trailer-number btn btn-min-md btn-primary', 'type'=> Route::currentRouteName() == 'trailer.list' ? 'submit' : 'button')) !!}
             </div>
         </div>
@@ -21,13 +21,13 @@
         <div class="mt-4 mb-4">Or</div>
         {!! Form::open(array('method' => 'GET', 'route' => 'trailer.list', 'class' => 'form', 'files'=>true, 'id' => 'search-by-vin-number')) !!}
         <div class="form-group row mb-0">
-            <label for="VehicleId_VIN" class="col-md-3 col-form-label text-md-right">{{ __('Enter VIN Number') }}</label>
-            <div class="col-md-3">
-                <div class="form-control-wrap search">
+            <label for="VehicleId_VIN" class="col-lg-3 col-form-label text-lg-right">{{ __('Enter VIN Number') }}</label>
+            <div class="col-md-6 col-lg-3">
+                <div class="form-control-wrap search mb-2 mb-md-0">
                     <input type="text" class="form-control form-control-radius" name="VehicleId_VIN" id="VehicleId_VIN" autocomplete="off" placeholder="VIN Number" value="{{\Request::get('VehicleId_VIN')}}">
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6 col-lg-3">
             {!! Form::button('Find', array('class'=>'search-by-vin-number btn btn-min-md btn-primary', 'type'=> Route::currentRouteName() == 'trailer.list' ? 'submit' : 'button')) !!}
             </div>
         </div>
@@ -36,13 +36,13 @@
         <div class="mt-4 mb-4">Or</div>
         {!! Form::open(array('method' => 'GET', 'route' => 'trailer.list', 'class' => 'form', 'files'=>true, 'id' => 'search-by-tracking-id')) !!}
         <div class="form-group row mb-0">
-            <label for="TrackingId" class="col-md-3 col-form-label text-md-right">{{ __('Tracking Number') }}</label>
-            <div class="col-md-3">
-                <div class="form-control-wrap search">
+            <label for="TrackingId" class="col-lg-3 col-form-label text-lg-right">{{ __('Tracking Number') }}</label>
+            <div class="col-md-6 col-lg-3">
+                <div class="form-control-wrap search mb-2 mb-md-0">
                     <input type="text" class="form-control form-control-radius" name="TrackingId" id="TrackingId" autocomplete="off" placeholder="Tracking Number" value="{{\Request::get('TrackingId')}}">
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6 col-lg-3">
             {!! Form::button('Find', array('class'=>'search-by-tracking-id btn btn-min-md btn-primary', 'type'=> Route::currentRouteName() == 'trailer.list' ? 'submit' : 'button')) !!}
             </div>
         </div>
@@ -53,11 +53,11 @@
         </header>
         {!! Form::open(array('method' => 'GET', 'route' => 'trailer.list', 'class' => 'form', 'files'=>true, 'id' => 'search-trailer')) !!}
         <div class="form-group row mb-5">
-            <div class="col-md-4">
-                {!! Form::select('business', ['' => '--All business System--']+$business, \Request::get('business') ? \Request::get('business') : null, array('class'=>'form-control form-control-radius', 'id'=>'business')) !!}
+            <div class="col-md-6 col-lg-4">
+                {!! Form::select('business', ['' => '--All business System--']+$business, \Request::get('business') ? \Request::get('business') : null, array('class'=>'form-control form-control-radius mb-2 mb-lg-0', 'id'=>'business')) !!}
             </div>
-            <div class="col-md-4">
-                {!! Form::select('SiteId', ['' => '--All Locations--']+$locations, \Request::get('SiteId') ? \Request::get('SiteId') : null, array('class'=>'form-control form-control-radius', 'id'=>'SiteId')) !!}
+            <div class="col-md-6 col-lg-4">
+                {!! Form::select('SiteId', ['' => '--All Locations--']+$locations, \Request::get('SiteId') ? \Request::get('SiteId') : null, array('class'=>'form-control form-control-radius mb-2 mb-lg-0', 'id'=>'SiteId')) !!}
             </div>
             <div class="col-md-4">
                 {!! Form::button('Find', array('class'=>'search-by-business-location btn btn-min-md btn-primary', 'type'=> Route::currentRouteName() == 'trailer.list' ? 'submit' : 'button')) !!}
