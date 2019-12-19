1417a4b84edfaebe24d2666b15ca29ca9bf129e7
Kashif Khan <m.kashifmit@gmail.com>
2019-12-19 23:42:32 +0500

    crone job time updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('track:trailer')->dailyAt('23:59');
+        $schedule->command('track:trailer')->dailyAt('00:00');
     }
 
     /**
