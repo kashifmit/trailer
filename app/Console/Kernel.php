526040d23c4b6c183dea7d4efda77bbbaff61237
Kashif Khan <m.kashifmit@gmail.com>
2020-01-07 16:00:57 +0500

    cron job settings

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('track:trailer')->dailyAt('00:00');
+        $schedule->command('track:trailer')->daily();
     }
 
     /**
